0aa9af179448e1693254569e879e1f8973ed1249
Adnan Mangaonkar <adnans0307@gmail.com>
2022-07-28 19:44:49 +0530

    changes

--- script.js
+++ script.js
@@ -114,3 +114,19 @@ const Footer = `
 
 document.querySelector(".Footer").innerHTML = Footer
 
+// counter
+$(document).ready(function() {
+
+    $('.counter').each(function () {
+$(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+}, {
+    duration: 4000,
+    easing: 'swing',
+    step: function (now) {
+        $(this).text(Math.ceil(now));
+    }
+});
+});
+
+});
