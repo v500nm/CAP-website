a6153ac8d5eef822a45c7f2a6258117f31e44896
Adnan Mangaonkar <adnans0307@gmail.com>
2022-08-04 23:02:08 +0530

    counter slide

--- pjs/carousal.js
+++ pjs/carousal.js
@@ -34,3 +34,15 @@ $(document).ready(function() {
         autoPlay:true
     });
 });
+$(document).ready(function() {
+    $("#news-slider3").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,4],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,    
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
