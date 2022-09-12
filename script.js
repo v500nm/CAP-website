959935e03fc4d64f5b0cb9a474212f349aab4c5c
Adnan Mangaonkar <adnans0307@gmail.com>
2022-09-12 10:33:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function preLoad(){
 const navbar = `
 <div class="topRow">
               <div class="topImg">
-                <a href="../index.html"><img src="../images/CAPlogo.png" class="navImage" height="120px" width="200px"></a>
+                <a href="../index.html"><img src="../images/CAPlogo.png" class="navImage" height="120px"></a>
               </div>
               <div class="text-center">
                 <h1 class="title word-break"><strong>Citizens for Animal Protection</strong></h1>
