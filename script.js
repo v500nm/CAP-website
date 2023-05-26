3b2edc8480576233497d72d1589aeab7e838c280
Adnan Mangaonkar <adnans0307@gmail.com>
2023-05-26 11:33:47 +0530

    Update script.js (#70)

--- script.js
+++ script.js
@@ -127,9 +127,7 @@ const Footer = `
                         <h5 class="text-white mb-3">
                            Locate Us!
                         </h5>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7532.608963159227!2d72.96618702407422!3d19.26912079372843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7bbeead1f4d67%3A0xa56561c00ccf69d4!2sFreedom%20Farm%2C%20Citizens%20for%20Animal%20Protection%20Foundation!5e0!3m2!1sen!2sin!4v1658849120003!5m2!1sen!2sin"
-                                width="300" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.3170012379073!2d72.9750259751643!3d19.26857598197604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7bbeead1f4d67%3A0xa56561c00ccf69d4!2sFreedom%20Farm%2C%20Citizens%20for%20Animal%20Protection%20Foundation!5e0!3m2!1sen!2sin!4v1685080990333!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
